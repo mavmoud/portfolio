b6ce8f066d2ed2b5d86b3bdad3c6297d5e2d73c0
Mahmoud Mohamed <mahmoud@null.net>
2024-07-14 11:16:41 +0100

    Fixed temporary mobile page overflow

--- src/components/Mobile.tsx
+++ src/components/Mobile.tsx
@@ -11,27 +11,29 @@ function Mobile() {
     return (
         <>
             <Fade direction="down" cascade triggerOnce>
-                <div className="mobileText">
-                    Mobile version is under construction.
-                </div>
-                <div className="mobileLinks">
-                    <div className="mLinks">
-                        <div
-                            className="mlink"
-                            onClick={() => handleClick("https://github.com/mavmoud")}
-                        >
-                            Github
-                        </div>
-                        <div
-                            className="mlink"
-                            onClick={() =>
-                                handleClick("https://www.linkedin.com/in/mavmoud/")
-                            }
-                        >
-                            Linkedin
-                        </div>
-                        <div className="mlink" onClick={emailClick}>
-                            Email
+                <div className="mobile">
+                    <div className="mobileText">
+                        Mobile version is under construction.
+                    </div>
+                    <div className="mobileLinks">
+                        <div className="mLinks">
+                            <div
+                                className="mlink"
+                                onClick={() => handleClick("https://github.com/mavmoud")}
+                            >
+                                Github
+                            </div>
+                            <div
+                                className="mlink"
+                                onClick={() =>
+                                    handleClick("https://www.linkedin.com/in/mavmoud/")
+                                }
+                            >
+                                Linkedin
+                            </div>
+                            <div className="mlink" onClick={emailClick}>
+                                Email
+                            </div>
                         </div>
                     </div>
                 </div>
