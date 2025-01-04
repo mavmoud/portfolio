beaa90674954c498e14a548edfc22163b42cfe92
Mahmoud Mohamed <mahmoud@null.net>
2025-01-03 20:23:37 -0500

    Changed favicon and added fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,8 +13,19 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        gray: "#939393",
+        silver: "#BBBBBB",
+      },
+      fontFamily: {
+        switzer: ["var(--font-switzer)"],
+        gambarino: ["var(--font-gambarino)"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
