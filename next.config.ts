3505e9a936a13726377f8af0cf8dd47f190ffdd9
Mahmoud Mohamed <mahmoud@null.net>
2025-02-26 02:59:00 -0500

    Created a reverse proxy for PostHog

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
